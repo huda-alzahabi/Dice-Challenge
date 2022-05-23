588eb73987a26ae4ff82b1db4624e2de483e0c1a
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-23 11:00:42 +0300

    Added the winning condition

--- scripts.js
+++ scripts.js
@@ -13,6 +13,13 @@ function start_game() {
     var d2 = document.getElementsByClassName("img2");
     roll_dice();
     d1[0].src = "images/dice" + random + ".png";
+    var r1 = random;
     roll_dice();
     d2[0].src = "images/dice" + random + ".png";
+    var r2 = random;
+
+    if (r1 > r2) document.getElementById("result").innerHTML = "Player1 Wins";
+    else if (r1 < r2)
+        document.getElementById("result").innerHTML = "Player2 Wins";
+    else document.getElementById("result").innerHTML = "Draw";
 }
