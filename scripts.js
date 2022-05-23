fcf64528a60dd27525ef8610c4bf05411616cb9d
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-23 10:53:42 +0300

    Added the startgame function to roll the dice

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 window.addEventListener("load", start_game, false);
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function getRandomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function roll_dice() {
-    random = getRandomInt(4);
+    random = getRandomNumberBetween(1, 6);
 }
 
 function start_game() {
-    var dice = document.getElementsByClassName("dice");
+    var d1 = document.getElementsByClassName("img1");
+    var d2 = document.getElementsByClassName("img2");
+    roll_dice();
+    d1[0].src = "images/dice" + random + ".png";
+    roll_dice();
+    d2[0].src = "images/dice" + random + ".png";
 }
